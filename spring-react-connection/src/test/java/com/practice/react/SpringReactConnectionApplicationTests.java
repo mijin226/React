0caa6ced3407ff45e7d0fe82e294ecba13d12ab3
mijin226 <rhalwls56@naver.com>
2025-02-01 21:57:22 +0900

    250201 22:00 React-Spring 연동 연습_Spring 속 React 세팅까지 성공. 의존주입 및 데이터 연결,포트 일관성이 관건

---
+++ spring-react-connection/src/test/java/com/practice/react/SpringReactConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.react;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactConnectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
